5743a4efb6057aa6acfb5997998b3b052f343e16
cytoly <cutolytic@gmail.com>
2021-06-02 03:59:39 +0200

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# last updated on 2021-06-02 at 01H:07M:50S
---
+++ commiter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+email="cutolytic@gmail.com"
+username="cytoly"
+quotes[0]="yes."
+quotes[1]="No."
+quotes[2]="shut up I need clout"
+quotes[3]="I still need clout"
+quotes[4]="clout clout clout"
+quotes[5]="Engineer gaming"
+quotes[6]="gaming"
+
+random_quote=${quotes[$RANDOM % ${#quotes[@]} ]}
+update=$(date '+%Y-%m-%d at %HH:%MM:%SS')
+echo "# last updated on $update" > README.md
+git config --global user.email $email
+git config --global user.name $username
+git commit -am $random_quote
